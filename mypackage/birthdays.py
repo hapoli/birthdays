c7af349dc7a90059e08829a05a9dfe21fbe75d71
sofiabenetti99 <sofia.benetti99@gmail.com>
2019-12-10 12:17:19 -0500

    issue4 done

--- mypackage/birthdays.py
+++ mypackage/birthdays.py
@@ -4,25 +4,31 @@ This is our birthday module
 
 """
 
-"""Set of famous people and their birthdays"""
+'''
+open the birth.csv which contains 
+'''
 
-birthdays = {
-    'Albert Einstein': '03/14/1879',
-    'Benjamin Franklin': '01/17/1706',
-    'Ada Lovelace': '12/10/1815',
-    'Donald Trump': '06/14/1946',
-    'Rowan Atkinson': '01/6/1955'}
+import csv
 
+reader = csv.reader (open('birth.csv', 'r'))
+d = {}
+for row in reader:
+    k,v = row  
+    d[k]= v
+
+"""
+functions
+"""
 def print_birthdays():
-    """Return the list of famous people's names"""
+    """Return famous people's names of the csv"""
     print('Welcome to the birthday dictionary. We know the birthdays of these people:')
-    for name in birthdays:
-        print(name)
+    for e in d:
+        print(e)
 
 def return_birthday(name):
     """Given a name, if found, return the birthdays"""
-    if name in birthdays:
-        print('{}\'s birthday is {}.'.format(name, birthdays[name]))
+    if name in d:
+        print('{}\'s birthday is {}.'.format(name, d[name]))
     else:
         print('Sadly, we don\'t have {}\'s birthday.'.format(name))
 
