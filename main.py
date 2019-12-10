f2d0c8491a0d8a0b5da9371d08e172e0574dcb87
hapoli <anna.poli@student.gmail.com>
2019-12-10 18:33:48 +0100

    issue5 done

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 #! /usr/bin/env python3
 
-from mypackage import birthdays
+from mypackage import return_birthday, birthdays
 import sys
+import argparse
 
-"""Given the user's input apply return_birthday. """
+'''Given one or more name return the birth date with different level of 
+verbosity if they are contained in the csv
+
+'''
+
+parser = argparse.ArgumentParser(
+         prog="This program returns the birthdays of famous people")
+parser.add_argument('n', nargs='+', 
+         help='Insert one or more name in the format: "Name Surname"')  
+parser.add_argument('-v', '--verbosity', action = 'count', default= 0,
+         help='Decide the level of verbosity')
+args = parser.parse_arg()
+
 
 if len(sys.argv)>1:
     birthdays.return_birthday(sys.argv[1])
