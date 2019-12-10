c08c16296d2b693f5c63c24a10a748c330bfb353
giuliabasaglia <giulia.basaglia@student.h-farm.com>
2019-12-10 14:07:54 -0500

    issue 6 done

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python3
-
-from mypackage import return_birthday, birthdays
 import sys
 import argparse
+
+#from birthdays import return_birthday
+from mypackage import birthdays
 
 '''Given one or more name return the birth date with different level of 
 verbosity if they are contained in the csv
@@ -15,10 +16,25 @@ parser.add_argument('n', nargs='+',
          help='Insert one or more name in the format: "Name Surname"')  
 parser.add_argument('-v', '--verbosity', action = 'count', default= 0,
          help='Decide the level of verbosity')
-args = parser.parse_arg()
+args = parser.parse_args()
 
 
-if len(sys.argv)>1:
-    birthdays.return_birthday(sys.argv[1])
-else:
-    print("You didn't pass any argument")
+'''Different output for different levels of verbosity
+
+'''
+
+# verbosity option
+name = args.n
+
+for i in name:
+    #if args.verbosity:
+    if birthdays.return_birthday(i):
+        if args.verbosity >= 2:
+            print('{} was born the {}'.format(i, birthdays.return_birthday(i)))
+        elif args.verbosity >= 1:
+            print ('{} : {}'.format(i, birthdays.return_birthday(i)))
+        else:
+            print (birthdays.return_birthday(i))
+    else:
+        print ('Sorry, {} is not in the list, '.format(i))
+        birthdays.print_birthdays()
--- mypackage/birthdays.py
+++ mypackage/birthdays.py
@@ -4,31 +4,32 @@ This is our birthday module
 
 """
 
-'''
-open the birth.csv which contains 
+'''Open the birth.csv which contains 
+
 '''
 
 import csv
 
-reader = csv.reader (open('birth.csv', 'r'))
+reader = csv.reader (open('mypackage/birth.csv', 'r'))
 d = {}
 for row in reader:
     k,v = row  
     d[k]= v
 
+"""Functions
+
 """
-functions
-"""
+
 def print_birthdays():
     """Return famous people's names of the csv"""
-    print('Welcome to the birthday dictionary. We know the birthdays of these people:')
+    print('We know the birthdays of these people:')
     for e in d:
         print(e)
 
 def return_birthday(name):
     """Given a name, if found, return the birthdays"""
     if name in d:
-        print('{}\'s birthday is {}.'.format(name, d[name]))
+        return d[name]
     else:
-        print('Sadly, we don\'t have {}\'s birthday.'.format(name))
+        return False 
 
